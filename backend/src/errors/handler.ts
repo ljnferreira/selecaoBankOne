86aa58274a82dad1ca428f1383bbb61f6ba97e24
Lucas Ferreira <ljnferreira@gmail.com>
2020-11-20 19:59:03 -0300

    refactor(backend): adding the error message to the client

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -22,7 +22,8 @@ const errorHandler: ErrorRequestHandler = (error, request, response, next ) => {
   console.error(error);
 
   return response.status(500).json({
-    message: 'Internal server error'
+    message: 'Internal server error',
+    error: error
   });
 
 }
