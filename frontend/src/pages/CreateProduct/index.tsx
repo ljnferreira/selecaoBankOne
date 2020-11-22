a6a0126d27e05fc802543093c14c2acbfadc5565
Lucas Ferreira <ljnferreira@gmail.com>
2020-11-21 21:33:35 -0300

    feat(frontend): created first set of routes to the app frontend

---
+++ frontend/src/pages/CreateProduct/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const CreateProduct: React.FC = () => {
+  return (
+    <Container>
+      <h1>Create</h1>
+    </Container>
+  );
+}
+
+export default CreateProduct;
---
+++ frontend/src/pages/ListProductById/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const ListProductById: React.FC = () => {
+  return (
+    <Container>
+      <h1>Details</h1>
+    </Container>
+  );
+}
+
+export default ListProductById;
