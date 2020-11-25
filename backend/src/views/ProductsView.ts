1e4e7e0e660a4dd4b7ac218a8e2dd5da72e61a39
Lucas Ferreira <ljnferreira@gmail.com>
2020-11-24 22:29:50 -0300

    chore(backend): removed comented code

--- backend/src/views/ProductsView.ts
+++ backend/src/views/ProductsView.ts
@@ -1,38 +1,14 @@
-import Product from "../models/Product";
-import Clothing from "../models/ClothingDetails";
-import Food from "../models/FoodDetails";
+import Product from "../classes/Product";
+import Clothing from "../classes/Clothing";
+import Food from "../classes/Food";
 
 export default {
-  render(product: Product){
-    // if(product.categoryId == 1){
-    //   let clothing = new Clothing();
-    //   clothing.name = product.name;
-    //   clothing.price = product.price;
-    //   clothing.registrationDate = product.registrationDate;
-    //   clothing.code = product.code;
-    //   clothing.description = product.description;
-    //   clothing.color = product.color;
-      
-    //   return clothing;
-    // }else{
-    //   let food = new Food();
-    //   food.name = product.name;
-    //   food.price = product.price;
-    //   food.registrationDate = product.registrationDate;
-    //   food.code = product.code;
-    //   food.perishable = product.perishable;
-    //   if (food.perishable){
-    //     food.validUntil = product.validUntil;
-    //   }
-    //   food.fabricationDate = product.fabricationDate;
-    //   food.measurementUnit = product.measurementUnit;
-      
-    //   return food;
-    // }
+  render(product: Product):Product{
+    console.log(product instanceof(Clothing));
     return product;
   },
 
-  renderMany(products: Product[]){
+  renderMany(products: Product[]):Product[]{
     return products.map(product => this.render(product));
   }
 }
