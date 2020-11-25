d79e4f39af7536e1d9d6c75c64354d656fd0e9e9
Lucas Ferreira <ljnferreira@gmail.com>
2020-11-24 22:31:20 -0300

    refactor(backend): refactored to use new function to get details of products and added method to handle new route

--- backend/src/controllers/ProductsController.ts
+++ backend/src/controllers/ProductsController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
-import {getRepository} from "typeorm";
-import ClothingDetails from "../models/ClothingDetails";
-import FoodDetails from "../models/FoodDetails";
+import {Between, getRepository} from "typeorm";
 
 import format from "date-fns/format";
 
-import Product from "../models/Product";
+import ProductModel from "../models/ProductModel";
+import ClothingDetails from "../models/ClothingDetails";
+import FoodDetails from "../models/FoodDetails";
+
 import ProductsView from "../views/ProductsView";
+import Product from "../classes/Product";
+import { getProductWithDetails } from "../utils/getProductWithDetails";
 
 export default {
   async create(request: Request, response: Response){
@@ -22,9 +25,9 @@ export default {
       validUntil
     } = request.body;
     
-    const registrationDate = format(new Date(Date.now()), 'dd/MM/yyyy')
+    const registrationDate = format(new Date(Date.now()), 'MM/dd/yyyy');
 
-    const productRepository = getRepository(Product);
+    const productRepository = getRepository(ProductModel);
     
     const productData = {
       name,
@@ -71,27 +74,40 @@ export default {
   },
 
   async index(request: Request, response: Response){
-    const productRepository = getRepository(Product);
+    const productRepository = getRepository(ProductModel);
 
     const products = await productRepository.find();
 
-    return response.json(ProductsView.renderMany(products));
+    const productsWithDetails: Product[] = await Promise.all(products.map(async (product) => {
+      return getProductWithDetails(product);
+    }))
+    
+    return response.json(ProductsView.renderMany(productsWithDetails));
   },
 
   async show(request: Request, response: Response){
     const { id } =request.params;
 
-    const productRepository = getRepository(Product);
+    const productRepository = getRepository(ProductModel);
 
-    const product = await productRepository.findOneOrFail(id);
+    try{
+      const product = await productRepository.findOneOrFail(id);
+      const productWithDetails = await getProductWithDetails(product);
 
-    return response.json(ProductsView.render(product));
+      return response.json(ProductsView.render(productWithDetails));
+    }catch(error){
+      return response.json({
+        message: "error, register not found",
+        error: error        
+      })
+    }
+    
   },
 
   async delete(request: Request, response: Response){
     const { id } = request.params;
 
-    const productRepository = getRepository(Product);
+    const productRepository = getRepository(ProductModel);
      
     try {
       let details;
@@ -113,7 +129,7 @@ export default {
         await productRepository.delete(id);
       } 
     } catch (error) {
-      return response.json({
+      return response.status(500).json({
         message: "error, register not found",
         error: error        
       })
@@ -123,4 +139,25 @@ export default {
       message: "deleted succesfully"
     });
   },
+
+  async filterByDate(request: Request, response: Response) {
+    const {
+      initialDate,
+      finalDate
+    } = request.body;
+
+    const initialObjectDate = new Date(initialDate);
+    const finalObjectDate = new Date(finalDate);
+
+    const productRepository = getRepository(ProductModel);
+    const products = await productRepository.find({
+      where:{
+        registrationDate: Between(initialObjectDate, finalObjectDate),
+      }
+    });
+
+    console.log(products);
+    return response.status(200).json(ProductsView.renderMany(products));
+    
+  }
 }
